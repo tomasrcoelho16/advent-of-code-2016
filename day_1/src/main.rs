c96ae02eb5fd51e3994e45aad9029e5ba92fd96a
tomasrcoelho <tomasrcoelho@gmail.com>
2025-03-31 13:53:30 +0100

    day 1 complete

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,5 +1,5 @@
-use std::env;
 use std::fs;
+use std::io;
 
 const DIRS: [(i32,i32); 4] = [
     (0,1), //NORTH
@@ -16,17 +16,14 @@ struct Information{
 }
 
 fn main() {
-    let args:Vec<String> = env::args().collect();
-    let file_name: &str = &args[1];
-    let content:String = read_file(file_name);
+    // let args:Vec<String> = env::args().collect();
+    // let file_name: &str = &args[1];
+    let content:String = read_file("input.txt").expect("Error reading file.");
     println!("The distance to Easter Bunny Headquarters is: {}", distance_to_ebhq(&content));
 }
 
-fn read_file(file_name: &str) -> String {
-
-    println!("Reading file: {file_name}");
-
-    fs::read_to_string(format!("inputs/{file_name}")).expect(&format!("Error finding file {file_name}."))
+fn read_file(file_name: &str) -> Result<String, io::Error> {
+    fs::read_to_string(format!("inputs/{file_name}"))
 }
 
 fn distance_to_ebhq(steps: &String) -> i32 {
@@ -43,7 +40,7 @@ fn distance_to_ebhq(steps: &String) -> i32 {
     for i in steps_iter{
         let dir = &i[0..1];
         let n_steps: i32 = (&i[1..]).trim().parse().expect("Error parsing to number.");
-        next_move(&mut info, dir, n_steps);
+        if next_move(&mut info, dir, n_steps){ break; }
     }
     info.horizontal_distance.abs() + info.vertical_distance.abs()
 }
@@ -54,12 +51,14 @@ fn next_move(info: &mut Information, dir: &str, n_steps: i32)-> bool {
     } else {
         (info.current_dir - 1).rem_euclid(DIRS.len() as i32)
     };
-    info.horizontal_distance += DIRS[info.current_dir as usize].0 * n_steps;
-    info.vertical_distance += DIRS[info.current_dir as usize].1 * n_steps;
-    let result = &info.visited.iter().any(|(x,y)| *x == info.horizontal_distance && *y == info.vertical_distance);
-    if *result {
-        return true;
+    for _ in 0..n_steps{
+        info.horizontal_distance += DIRS[info.current_dir as usize].0;
+        info.vertical_distance += DIRS[info.current_dir as usize].1;
+        let is_hq = &info.visited.iter().any(|(x,y)| *x == info.horizontal_distance && *y == info.vertical_distance);
+        if *is_hq {
+            return true;
+        }
+        (&mut info.visited).push((info.horizontal_distance, info.vertical_distance));
     }
-    (&mut info.visited).push((info.horizontal_distance, info.vertical_distance));
     false
 }
